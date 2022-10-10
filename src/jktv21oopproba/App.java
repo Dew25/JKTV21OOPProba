befe73346b79a2dc4f89632a247b470277b80328
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2022-10-10 10:18:33 +0300

    Сделали рефакторинг кода с целью упрощения восприятия логики приложения

--- src/jktv21oopproba/App.java
+++ src/jktv21oopproba/App.java
@@ -6,6 +6,7 @@
 package jktv21oopproba;
 
 
+import java.util.Arrays;
 import myclasses.Book;
 import tools.Tools;
 
@@ -16,9 +17,16 @@ import tools.Tools;
 public class App {
     public void run(){
         Tools tools = new Tools();
-        Book book = tools.createBook("Book Ivana");
-        book.addAuthor(tools.createAuthor("Ivan","Ivanov"));
-        System.out.println(book.toString());
+        Book[] books = new Book[5];
+        for (int i = 0; i < 2; i++){
+            books[i] = tools.createBook();
+        }
+        System.out.println(Arrays.toString(books)); 
+        
+        for (int i = 0; i < 2; i++) {
+            System.out.printf("Book{%n\t%s%n\tAuthors=[%s]%n}",books[i].getTitle(),Arrays.toString(books[i].getAuthors()));
+        }
+        
     }
    
 }
